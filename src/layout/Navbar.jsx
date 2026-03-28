3fe0d0baf00452ad7aff6befddbc48af51176e68
chaiiiiy <clepilepsia@gmail.com>
2026-03-29 00:12:34 +0800

    feat: add scroll logic to NavBar, refactor mobile menu

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect, use } from "react";
 import { Button } from "@/components/Button";
 import { Menu, X } from "lucide-react";
 
 const navLinks = [
+  { href: "#", label: "Home" },
   { href: "#about", label: "About" },
   { href: "#skills", label: "Skills" },
   { href: "#projects", label: "Projects" },
@@ -10,9 +11,24 @@ const navLinks = [
 
 export const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
-    <header className="fixed top-0 left-0 right-0 bg-transparent py-5">
+    <header
+      className={`fixed top-0 left-0 right-0 transition-all  ${
+        isScrolled ? "glass-strong py-3" : "bg-transparent py-5"
+      } z-50`}
+    >
       <nav className="container mx-auto px-6 flex items-center justify-between">
         <a
           href="#"
@@ -38,12 +54,14 @@ export const Navbar = () => {
 
         {/* CTA Button */}
         <div className="hidden md:block">
-          <Button size="sm">Contact Me</Button>
+          <Button size="sm" className="nav-button">
+            Contact Me
+          </Button>
         </div>
 
         {/* Mobile Menu Button */}
         <button
-          className="md:hidden p-2 text-foreground"
+          className="md:hidden p-2 text-foreground z-50"
           onClick={() => setIsMobileMenuOpen((prev) => !prev)}
         >
           {isMobileMenuOpen ? <X size={20} /> : <Menu size={20} />}
@@ -52,19 +70,20 @@ export const Navbar = () => {
 
       {/* Mobile Menu */}
       {isMobileMenuOpen && (
-        <div className="md:hidden glass-strong animate-fade-in mt-4">
-          <div className="container mx-auto px-6 py-6 flex flex-col gap-3">
+        <div className="fixed inset-0 bg-background/90 backdrop-blur-md z-40 flex flex-col items-center justify-center transition-all duration-300 pointer-events-auto">
+          <div className="flex flex-col space-y-8">
             {navLinks.map((link) => (
               <a
                 key={link.href}
                 href={link.href}
-                className="text-md text-muted-foreground hover:text-foreground py-2"
+                className="text-md text-center text-muted-foreground hover:text-primary transition-colors duration-300"
+                onClick={() => setIsMobileMenuOpen(false)}
               >
                 {link.label}
               </a>
             ))}
 
-            <Button>Contact Me</Button>
+            <Button className="nav-button">Contact Me</Button>
           </div>
         </div>
       )}
