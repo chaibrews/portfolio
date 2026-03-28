4a512e0491d83734e1995b71f49243ef793b7a60
chaiiiiy <clepilepsia@gmail.com>
2026-03-28 16:13:58 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Navbar } from "@/layout/Navbar";
+import { Hero } from "@/sections/Hero";
+import { About } from "@/sections/About";
+import { Experience } from "@/sections/Experience";
+import { Contact } from "@/sections/Contact";
+
+function App() {
+  return (
+    <div className="min-h-screen overflow-x-hidden">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Experience />
+        <Contact />
+      </main>
+    </div>
+  );
+}
+
+export default App;
